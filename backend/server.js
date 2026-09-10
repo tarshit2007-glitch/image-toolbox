f029a3fd1ee08d4901527ea24c3b9de0124e8dd4
tarshit2007-glitch <tarshit2007@gmail.com>
2026-09-10 23:23:48 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -357,7 +357,7 @@ app.use((error, req, res, next) => {
 // START SERVER
 // ------------------------------
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
